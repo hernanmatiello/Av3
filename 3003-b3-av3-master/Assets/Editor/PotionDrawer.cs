c840140adf0f60af5add5a734e90f108c2a7e840
Hernan Matiello <hernan.matiello@cejoseleitelopes.com.br>
2017-09-22 10:35:08 -0300

    First Commit
    Scripts bases

---
+++ 3003-b3-av3-master/Assets/Editor/PotionDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PotionSelectorAttribute))]
+public class PotionDrawer : PropertyDrawer {
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		string[] potionsName = new string[PotionDB.Instance.potions.Length];
+
+		for (int x = 0; x < PotionDB.Instance.potions.Length; x++) {
+			potionsName [x] = PotionDB.Instance.potions [x].name;
+		}
+
+		int indicePotions = EditorGUI.Popup (
+			position, 
+			"Potions", 
+			ArrayUtility.IndexOf(potionsName, property.stringValue), 
+			potionsName);
+
+		property.stringValue = PotionDB.Instance.potions[indicePotions].name;
+	}
+}
---
+++ 3003-b3-av3-master/Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+	
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+	[PotionSelector]
+	public string potionName;
+
+	int indice;
+
+	public void Start()
+	{
+		for (int x = 0; x < PotionDB.Instance.potions.Length; x++)
+		{
+			if (PotionDB.Instance.potions [x].name == potionName) {
+				Debug.Log (PotionDB.Instance.potions [x].name + ": +" + PotionDB.Instance.potions [x].heal + " hp");
+			}
+		}
+	}
+}
